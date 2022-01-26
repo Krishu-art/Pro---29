841cbd882bfabe032596e44c11cd4139f43b2e7d
Krishu-art <75996418+Krishu-art@users.noreply.github.com>
2022-01-26 20:50:53 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -8,16 +8,29 @@ class Box {
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        this.visiblity = 255;
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
+        fill("blue")
+        console.log(this.body.speed);
+        if(this.body.speed < 3){
+       // super.display();
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
         rectMode(CENTER);
-        fill("blue")
         rect( 0, 0, this.width, this.height);
         pop();
+
+        }
+        else{
+            World.remove(world, this.body);
+          //  push();
+          //  this.visiblity = this.visiblity - 50;
+          //  tint(255,this.visiblity);
+          //  rect(this.body.position.x, this.body.position.y, 60, 70);
+          //  pop();
+           }
       }
 }
+
