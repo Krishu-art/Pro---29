9bb601feccd6981ae31bd8ff25164151c58528c7
Krishu-art <75996418+Krishu-art@users.noreply.github.com>
2022-01-27 13:28:33 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -13,24 +13,11 @@ class Box {
       }
       display(){
         fill("blue")
-        console.log(this.body.speed);
-        if(this.body.speed < 3){
-       // super.display();
         push();
         translate(this.body.position.x, this.body.position.y);
         rectMode(CENTER);
         rect( 0, 0, this.width, this.height);
-        pop();
-
-        }
-        else{
-            World.remove(world, this.body);
-          //  push();
-          //  this.visiblity = this.visiblity - 50;
-          //  tint(255,this.visiblity);
-          //  rect(this.body.position.x, this.body.position.y, 60, 70);
-          //  pop();
-           }
+        pop();  
       }
 }
 
